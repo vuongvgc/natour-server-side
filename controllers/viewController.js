d1619b3bbd4a742ad10ecceaa346ba7e94971c6a
vuongvgc <vuongvgc@gmail.com>
2021-10-05 21:26:16 +0700

    Feature: Building overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,17 @@
-exports.getOverView = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverView = catchAsync(async (req, res) => {
+  // 1 Get tour from DB
+  const tours = await Tour.find();
+  // 2 Build template
+  // 3 Render template using date form step one
+
   return res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 exports.getTour = (req, res) => {
   return res.status(200).render('tour', {
     title: 'The Forest Hiker'
