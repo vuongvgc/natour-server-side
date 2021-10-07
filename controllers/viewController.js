ed53f80b7c7ad75034a6e175c29822eb5c57c39d
vuongvgc <vuongvgc@gmail.com>
2021-10-07 07:09:43 +0700

    Build tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,15 +5,24 @@ exports.getOverView = catchAsync(async (req, res) => {
   // 1 Get tour from DB
   const tours = await Tour.find();
   // 2 Build template
-  // 3 Render template using date form step one
+  // 3 Render template using data form step one
 
   return res.status(200).render('overview', {
     title: 'All Tours',
     tours
   });
 });
-exports.getTour = (req, res) => {
-  return res.status(200).render('tour', {
-    title: 'The Forest Hiker'
+exports.getTour = catchAsync(async (req, res) => {
+  // 1 Find tour detail, review, guide
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+  // 2 Build template
+  console.log(tour);
+  // 3 Reder template using data from step one
+  return res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+    tour
+  });
+});
